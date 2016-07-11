edaca6e2befc46cc83d77f52f9f5860830f5c19e
llwang8 <wangxbl56@gmail.com>
2016-07-11 12:12:11 -0400

    implement add link form, style and animation

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ $(document).ready(function(){
          .closest('.news-item')
          .addClass('is-liked');
     });
+
+    $('.js-add-link').on('click', function(event){
+        event.preventDefault();
+
+        $('.js-form').toggleClass('is-visible');
+    });
+
 });
 
 
